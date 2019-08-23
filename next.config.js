ecc42eda3569f65d0081f003739c1c3e60ce78c5
Jerry Green <jerrygreenest@gmail.com>
2019-08-23 16:33:07 +0500

    few changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const withReactSvg = require('next-react-svg')
 const { resolve } = require('path')
 
 module.exports = withReactSvg({
-  include: resolve('./assets'),
-  webpack(webpackConfig) {
-    webpackConfig.resolve.alias['~'] = resolve('./src')
-    webpackConfig.resolve.alias['@assets'] = resolve('./assets')
-    return webpackConfig
+  include: resolve('assets'),
+  webpack(config) {
+    config.resolve.alias['~'] = resolve('src')
+    config.resolve.alias['@assets'] = resolve('assets')
+    return config
   }
 })
